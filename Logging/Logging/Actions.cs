b5ad0108fa0f53ef5a4547edbc8e3fe0f35062a5
JuliaVn <juliya14725@gmail.com>
2022-05-18 15:33:25 +0300

    Logging task done

---
+++ Logging/Logging/Actions.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    class Actions
+    {
+        private static readonly Logger logger = Program.Logger;
+        public static string AddDotsToEnd(string s)
+        {
+            logger.Trace("Adding dots to the end of the string.");
+            return s + "...";
+        }
+        public static string RemoveAllSpacesFromEnd(string s)
+        {
+            logger.Trace("Removing all spaces from the end of the string.");
+            string temp = s;
+            while (temp[^1] == ' ')
+            {
+                temp = s.Remove(s.Length - 1);
+            }
+            return temp;
+        }
+        public static string CutString(string s)
+        {
+            if (s.Length < 12)
+            {
+                logger.Trace("Returned the string since there are less than 12 symbols.");
+                return s;               
+            }
+            else
+            {
+                logger.Trace("Cutting the string till 12th symbol.");
+                return s[..12];
+            }          
+        }
+    }
+}
---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    class Program
+    {
+        delegate string Remaking(string s);
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        public static Logger Logger => logger;
+        public static void Main()
+        {
+            try
+            {
+                logger.Info("App is launching.");
+                Remaking re = null;
+                re += Actions.CutString;
+                re += Actions.RemoveAllSpacesFromEnd;
+                re += Actions.AddDotsToEnd;
+
+                Console.Write("Enter the line:  ");
+                string line = Console.ReadLine();
+
+                foreach (Remaking d in re.GetInvocationList())
+                {
+                    line = d.Invoke(line);
+                }
+                Console.WriteLine($"\nResult: {line}");
+                logger.Info("The operations have been finished.");
+            }
+            catch (Exception e)
+            {
+                logger.Error($"Something went wrong.Exception occured: {e.Message}");
+            }
+            LogManager.Shutdown();
+        }
+    }
+}
